5c070847b0725c61f0a3a16c27a6e02178a51d2c
Suresh Kumar <suresh.kumar@rebtel.com>
2015-11-12 12:34:22 +0100

    app skeleton added

---
+++ TestApp/app/src/main/java/se/svt/sureshkumar/testapp/util/Constant.java
@@ -0,0 +1,10 @@
+package se.svt.sureshkumar.testapp.util;
+
+import se.svt.sureshkumar.testapp.BuildConfig;
+
+/**
+ * Created by suresh.kumar on 12/11/15.
+ */
+public class Constant {
+	public static boolean DEBUG = BuildConfig.DEBUG;
+}
